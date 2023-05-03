6f570355d720dadda45e91a316eb1695336eec21
Nicholas Schulz <103540498+BubbaHexinator@users.noreply.github.com>
2023-05-03 14:48:44 -0400

    create_Practice.py

---
+++ practice.py
@@ -0,0 +1,21 @@
+# Create three dictionaries with classmates' names as keys, location, years and tech as values.
+classmate_city = {
+    "Anna": "London",
+    "Mohammad": "Hong Kong",
+    "Viral": "Seattle",
+    "Lizette": "Chicago"
+}
+
+classmate_years = {
+    "Anna": 7,
+    "Mohammad": 0,
+    "Viral": 5,
+    "Lizette": 2
+}
+
+classmate_tech = {
+    "Anna": "Java",
+    "Mohammad": "Microsoft Office",
+    "Viral": "C#",
+    "Lizette": "JavaScript"
+}
